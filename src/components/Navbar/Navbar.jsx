bc229229b9446bb8bbe1131ba753443800672dfa
Ayomide <ayomidedans@gmail.com>
2022-03-18 00:30:28 +0100

    deploy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,15 +13,15 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">9 Crowther St, Ahmadu Bello road, Surulere Lagos, Nigeria</p>
+        <p className="p__opensans">+234 802-344-1230</p>
+        <p className="p__opensans">+234 802-555-1237</p>
       </div>
 
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} alt="img" />
         <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
@@ -39,7 +39,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">&copy;2022 Gericht. All Rights reserved.</p>
     </div>
 
   </div>
